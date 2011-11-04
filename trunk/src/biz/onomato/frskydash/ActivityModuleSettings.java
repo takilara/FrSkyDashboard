32c7bc548cc946fb87b80aacb41f75c5f04412be
eso <eso@64be4f48-a600-4b3c-a018-ccb7a4f33b57>
2011-11-04 23:31:28 +0000

    refs #105

--- trunk/src/biz/onomato/frskydash/ActivityModuleSettings.java
+++ trunk/src/biz/onomato/frskydash/ActivityModuleSettings.java
@@ -2,9 +2,15 @@ package biz.onomato.frskydash;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ActivityModuleSettings extends Activity {
+public class ActivityModuleSettings extends Activity implements OnItemSelectedListener {
 
 	private static final String TAG = "FrSky-Settings";
     
@@ -13,19 +19,16 @@ public class ActivityModuleSettings extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_modulesettings);
 		
-		//globals = ((MyApp)getApplicationContext());
-
-        //float newVal = globals.setChannelById(AD1, 200);
-        
-        //oAd1 = globals.getChannelById(0);
-        
-        
-        TextView test = (TextView) findViewById(R.id.textView1);
-		//test.setText(Float.toString(oAd1.getValue()));
-        //test.setText(oAd1.getValue().toPlainString());
-        //test.setText(oAd1.toString());
+		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.alarm_level, android.R.layout.simple_spinner_item );
+		adapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item);
+			 
+		Spinner s = (Spinner) findViewById( R.id.FrSkySettings_spinner_level );
+		s.setAdapter( adapter );
+		s.setOnItemSelectedListener(this);
 		
-        
+				
+				
+				
 	}
 	
 	@Override
@@ -35,4 +38,26 @@ public class ActivityModuleSettings extends Activity {
 		TextView test = (TextView) findViewById(R.id.textView1);
 		//test.setText(oAd1.toString());
 	}
+	
+	
+
+
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos,
+			long id) {
+		// TODO Auto-generated method stub
+		Toast.makeText(parent.getContext(), "The planet is " +
+		          parent.getItemAtPosition(pos).toString(), Toast.LENGTH_LONG).show();
+		
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
+
+

57629779821fce84a15da84096c368a89b564f3b
hans <hans@64be4f48-a600-4b3c-a018-ccb7a4f33b57>
2012-04-17 11:40:06 +0000

    refs #307 implemented translation of new voltage sensor data

--- src/biz/onomato/frskydash/hub/ChannelTypes.java
+++ src/biz/onomato/frskydash/hub/ChannelTypes.java
@@ -6,6 +6,8 @@ package biz.onomato.frskydash.hub;
  */
 public enum ChannelTypes {
 
-	undefined, gps_altitude_before, gps_altitude_after, temp1, rpm, fuel, temp2, volt, altitude_before, altitude_after, gps_speed_before, gps_speed_after, longitude_before, longitude_after, ew, latitude_before, latitude_after, ns, course_before, course_after, day, month, year, hour, minute, second, acc_x, acc_y, acc_z;
+	undefined, gps_altitude_before, gps_altitude_after, temp1, rpm, fuel, temp2, volt, altitude_before, altitude_after, gps_speed_before, gps_speed_after, longitude_before, longitude_after, ew, latitude_before, latitude_after, ns, course_before, course_after, day, month, year, hour, minute, second, acc_x, acc_y, acc_z,
+	// for the new volt sensor we need a voltage value per cell (up to 6 cell)
+	volt_1, volt_2, volt_3, volt_4, volt_5, volt_6;
 
 }

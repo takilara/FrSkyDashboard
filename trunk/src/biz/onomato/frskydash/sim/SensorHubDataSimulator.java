01897d79282917b3cedd1077e05022e0687a9948
hans <hans@64be4f48-a600-4b3c-a018-ccb7a4f33b57>
2012-04-24 13:58:30 +0000

    refs #400 still requires proper shutdown of thread on closure of app and state onresume of sim activity isn't correct if thread has been started and stopped. Start/stop on itself works now. Also updated the way the file is handled.

---
+++ trunk/src/biz/onomato/frskydash/sim/SensorHubDataSimulator.java
@@ -0,0 +1,13 @@
+package biz.onomato.frskydash.sim;
+
+/**
+ * should create sensor hub data frames for simulation
+ * 
+ * @author hcpl
+ * 
+ */
+public class SensorHubDataSimulator {
+
+	// TODO
+
+}

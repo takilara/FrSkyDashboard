b8dc5e1279de7eb9b8eca942cbabd70b45554cf4
hans <hans@64be4f48-a600-4b3c-a018-ccb7a4f33b57>
2012-06-20 16:23:05 +0000

    closes #444 handling only the user bytes. As long as the Channel broadcast fix (refs #446 ) isn't implemented we still need to pass the server instance though

--- trunk/src/biz/onomato/frskydash/hub/FrSkyHub.java
+++ trunk/src/biz/onomato/frskydash/hub/FrSkyHub.java
@@ -65,128 +65,6 @@ public class FrSkyHub extends Hub{
 		initializeChannels();
 	}
 
-	
-	// FIXME: shall be deprecated when addUserBytes is properly implemented
-	/**
-	 * Extract user data bytes from telemetry data frame. This subframe is
-	 * delimited by the 0x5E byte, included in the frame argument for this
-	 * method. A single call can contain incomplete user data frames so need to
-	 * keep track of previous frame also.
-	 * 
-	 * 
-	 * @param frame
-	 */
-	public void extractUserDataBytes(FrSkyServer paramServer, Frame frame) {
-		// init
-		// FIXME can't this be implemented in another way so we don't have to
-		// pass the server instance?
-		// eso: server is currently used for broadcasts from the hub, this shall be removed
-		// as the hub shall not broadcast
-		server = paramServer;
-		//int b;
-		//int[] ints = frame.toInts();
-		//int[] ints = frame.getUserBytes();
-		// iterate elements in frame
-		// for (int b : frame) {
-		// don't handle all the bytes, skip header (0), prim(1), size(2),
-		// unused(3) and end but(10)
-		// the byte at index 2 indicated how many bytes are valid in this frame.
-		// Make sure to only take in account these valid bytes starting to count
-		// from byte at index 4
-		// index => byte description
-		// 0 => frame start byte
-		// 1 => type of frame byte (analog, signal, user data, ...)
-		// 2 => length of valid bytes
-		// 3 => discard this byte
-		// 4 => first user data byte
-		// ...
-		// 10 => stop byte frame
-		//int nrOfValidBytesInFrame = ints[2];
-		// for (int i = 4; i < Frame.SIZE_TELEMETRY_FRAME - 1; i++) {
-		//for (int i = 4; i < 4 + nrOfValidBytesInFrame; i++) {
-		for(int b : frame.getUserBytes()){
-			//b = ints[i];
-			// handle byte stuffing first
-			if (b == STUFFING_HUB_FRAME) {
-				hubXOR = true;
-				// drop this byte
-				continue;
-			}
-			if (hubXOR) {
-				b ^= XOR_HUB_FRAME;
-				// don't unset the xor flag yet since we'll have to check on
-				// this for start/stop bit detection
-				// xor = false;
-				Logger.d(TAG, "XOR operation, unstuffed to "
-						+ Integer.toHexString(b));
-			}
-			// if we encounter a start byte we need to indicate we're in a
-			// frame or if at the end handle the frame and continue
-			if (b == START_STOP_HUB_FRAME && !hubXOR) {
-				// if currentFrameIndex is not set we have to start a new
-				// frame here
-				if (currentHubFrameIndex < 0) {
-					// init current frame index at beginning
-					currentHubFrameIndex = 0;
-					// and copy this first byte in the frame
-					hubFrame[currentHubFrameIndex++] = b;
-				}
-				// otherwise we were already collecting a frame so this
-				// indicates we are at the end now. At this point a frame is
-				// available that we can send over.
-				else if (currentHubFrameIndex == SIZE_HUB_FRAME - 1) {
-					// just complete the frame we were collecting
-					hubFrame[currentHubFrameIndex] = b;
-					// this way the length is confirmed
-					handleHubDataFrame(hubFrame);
-					// once information is handled we can reset the frame
-					hubFrame = new int[SIZE_HUB_FRAME];
-					currentHubFrameIndex = 0;
-					// unlike the telemetry frames where 126 is on each side of
-					// the frame (126, x, y, 126, 126, x, y, 126) the 94 bit of
-					// the hub frame is only a single bit in between (94, x, y,
-					// 94, x, y, 94) so we need to reuse that last 94 bit
-					// encountered as the beginning of our next frame
-					hubFrame[currentHubFrameIndex++] = b;
-				}
-				// if for some reason we got 2 0x5e bytes after each other
-				// or the size of the frame was different we can't do
-				// anything with the previous collected information. We can
-				// log a debug message and drop the frame to start over
-				// again.
-				else {
-					// log debug info here
-					Logger.d(
-							TAG,
-							"Start/stop byte at wrong position: 0x"
-									+ Integer.toHexString(b)
-									+ " frame so far: "
-									+ Arrays.toString(hubFrame));
-					currentHubFrameIndex = 0;
-					hubFrame = new int[SIZE_HUB_FRAME];
-					hubFrame[currentHubFrameIndex++] = b;
-				}
-			}
-			// otherwise we are handling a valid byte that has to be put in
-			// the frame we are collecting. But only when we are currently
-			// working on a frame!
-			else if (currentHubFrameIndex >= 0
-					&& currentHubFrameIndex < SIZE_HUB_FRAME - 1) {
-				hubFrame[currentHubFrameIndex++] = b;
-			}
-			// finally it's possible that we receive bytes without being in
-			// a frame, just discard them for now
-			else {
-				// log debug info here
-				Logger.d(TAG,
-						"Received data outside frame, dropped byte: 0x"
-								+ Integer.toHexString(b));
-			}
-			// make sure to unset the xor flag at this point
-			hubXOR = false;
-		}
-	}
-
 	/**
 	 * once we extracted the user data frames these can be handled by checking
 	 * their data ID
@@ -231,71 +109,6 @@ public class FrSkyHub extends Hub{
 		// and update channel
 		else
 			updateChannel(sensorType, value);
-
-		/*
-		 * 
-		 * // check data ID and update correct channel switch (frame[1]) { case
-		 * 0x01: updateChannel(SensorTypes.gps_altitude_before,
-		 * getSignedLE16BitValue(frame)); break; case 0x01 + 8:
-		 * updateChannel(SensorTypes.gps_altitude_after,
-		 * getUnsignedLE16BitValue(frame)); break; case 0x02:
-		 * updateChannel(SensorTypes.temp1, getSignedLE16BitValue(frame));
-		 * break; case 0x03: // actual RPM value is Frame1*60 // also needs to
-		 * be divided by the number of blades of the prop! (=> // TODO for
-		 * settings) updateChannel(SensorTypes.rpm,
-		 * getUnsignedLE16BitValue(frame) * 60); break; case 0x04:
-		 * updateChannel(SensorTypes.fuel, getUnsignedLE16BitValue(frame));
-		 * break; case 0x05: updateChannel(SensorTypes.temp2,
-		 * getSignedLE16BitValue(frame)); case 0x06: // first 4 bit is battery
-		 * cell number // last 12 bit refer to voltage range 0-2100
-		 * corresponding 0-4.2V int cell = getBatteryCell(frame); // if (cell <
-		 * 1 || cell > 6) { if (cell < 0 || cell > 5) {
-		 * Logger.d(this.getClass().toString(),
-		 * "failed to handle cell nr out of range: " + cell); break; } double
-		 * value = getCellVoltage(frame); // get the right channeltype based on
-		 * cell nr String channelType = "volt_" + cell; // and update
-		 * updateChannel(SensorTypes.valueOf(channelType), value); break; case
-		 * 0x10: updateChannel(SensorTypes.altitude_before,
-		 * getSignedLE16BitValue(frame)); break; case 0x21:
-		 * updateChannel(SensorTypes.altitude_after,
-		 * getUnsignedLE16BitValue(frame)); break; case 0x11:
-		 * updateChannel(SensorTypes.gps_speed_before,
-		 * getUnsignedLE16BitValue(frame)); break; case 0x11 + 8:
-		 * updateChannel(SensorTypes.gps_speed_after,
-		 * getUnsignedLE16BitValue(frame)); break; case 0x12:
-		 * updateChannel(SensorTypes.longitude_before,
-		 * getUnsignedBE16BitValue(frame)); break; case 0x12 + 8:
-		 * updateChannel(SensorTypes.longitude_after,
-		 * getUnsignedBE16BitValue(frame)); break; case 0x1A + 8:
-		 * updateChannel(SensorTypes.ew, getUnsignedLE16BitValue(frame)); break;
-		 * case 0x13: updateChannel(SensorTypes.latitude_before,
-		 * getUnsignedBE16BitValue(frame)); break; case 0x13 + 8:
-		 * updateChannel(SensorTypes.latitude_after,
-		 * getUnsignedBE16BitValue(frame)); break; case 0x1B + 8:
-		 * updateChannel(SensorTypes.ns, getUnsignedLE16BitValue(frame)); break;
-		 * case 0x14: // should be between 0 & 359.99 degrees?? // FIXME seems
-		 * to work but with much noise updateChannel(SensorTypes.course_before,
-		 * getUnsignedLE16BitValue(frame)); break; case 0x14 + 8:
-		 * updateChannel(SensorTypes.course_after,
-		 * getUnsignedLE16BitValue(frame)); break; case 0x15:
-		 * updateChannel(SensorTypes.day, frame[2]);
-		 * updateChannel(SensorTypes.month, frame[3]); break; case 0x16:
-		 * updateChannel(SensorTypes.year, 2000 + frame[2]); break; case 0x17:
-		 * updateChannel(SensorTypes.hour, frame[2]);
-		 * updateChannel(SensorTypes.minute, frame[3]); break; case 0x18:
-		 * updateChannel(SensorTypes.second, frame[2]); break; case 0x24: //
-		 * actual 3-axis value is Frame1/1000 updateChannel(SensorTypes.acc_x,
-		 * getSignedLE16BitValue(frame) / 1000); break; case 0x25: // actual
-		 * 3-axis value is Frame1/1000 updateChannel(SensorTypes.acc_y,
-		 * getSignedLE16BitValue(frame) / 1000); break; case 0x26: // actual
-		 * 3-axis value is Frame1/1000 updateChannel(SensorTypes.acc_z,
-		 * getSignedLE16BitValue(frame) / 1000); break; default: // TODO add
-		 * current sensor data Logger.d(FrSkyServer.TAG,
-		 * "Unknown sensor type for frame: " + Arrays.toString(frame));
-		 * 
-		 * 
-		 * }
-		 */
 	}
 
 	/**
@@ -505,36 +318,9 @@ public class FrSkyHub extends Hub{
 	public static double alt_after = 0;
 
 	/**
-	 * Treemap to hold the Hubs channels
-	 */
-	//private static TreeMap<Integer, Channel> _sourceChannelMap;
-
-	/**
-	 * 
-	 * @return a ServerChannel corresponding to the given id
-	 */
-//	public static Channel getSourceChannel(int id) {
-//		return _sourceChannelMap.get(id);
-//	}
-
-	/**
-	 * TODO Should not be static since this relies on data initialised in the
-	 * ctor, need to review this.
-	 * 
-	 * @return all the ServerChannels in a TreeMap (key is the id of the
-	 *         Channel)
-	 */
-//	public TreeMap<Integer, Channel> getSourceChannels() {
-//		return _sourceChannelMap;
-//	}
-
-	/**
 	 * Create the _sourceChannelMap Populate it with our channels
 	 */
 	protected void initializeChannels() {
-//		_sourceChannelMap = new TreeMap<Integer, Channel>(
-//				Collections.reverseOrder());
-
 		// Sets up the hardcoded channels (Altitude,RPM)
 		// TODO: Figure out how to deal with race conditions on "split numbers"
 		// TODO create enums with descruption etc or proper factory instead
@@ -594,17 +380,108 @@ public class FrSkyHub extends Hub{
 		return HUB_ID;
 	}
 
-	/* (non-Javadoc)
-	 * @see biz.onomato.frskydash.hub.Hub#addUserBytes(int[])
-	 */
 	@Override
-	public void addUserBytes(int[] ints) {
-		// FIXME incomplete
+	public void addUserBytes(FrSkyServer server, int[] ints) {
 		// shall take user bytes
 		// decode (destuff) them, and add to internal queue
 		// then identify hub frames within this queue
 		// and pass them to handleHubDataFrame
-		
+		// FIXME can't this be implemented in another way so we don't have to
+		// pass the server instance?
+		// eso: server is currently used for broadcasts from the hub, this shall
+		// be removed as the hub shall not broadcast
+		// => server param to be removed
+		this.server = server;
+		// don't handle all the bytes, skip header (0), prim(1), size(2),
+		// unused(3) and end but(10)
+		// the byte at index 2 indicated how many bytes are valid in this frame.
+		// Make sure to only take in account these valid bytes starting to count
+		// from byte at index 4
+		// index => byte description
+		// 0 => frame start byte
+		// 1 => type of frame byte (analog, signal, user data, ...)
+		// 2 => length of valid bytes
+		// 3 => discard this byte
+		// 4 => first user data byte
+		// ...
+		// 10 => stop byte frame
+		for (int b : ints) {
+			// b = ints[i];
+			// handle byte stuffing first
+			if (b == STUFFING_HUB_FRAME) {
+				hubXOR = true;
+				// drop this byte
+				continue;
+			}
+			if (hubXOR) {
+				b ^= XOR_HUB_FRAME;
+				// don't unset the xor flag yet since we'll have to check on
+				// this for start/stop bit detection
+				// xor = false;
+				Logger.d(TAG,
+						"XOR operation, unstuffed to " + Integer.toHexString(b));
+			}
+			// if we encounter a start byte we need to indicate we're in a
+			// frame or if at the end handle the frame and continue
+			if (b == START_STOP_HUB_FRAME && !hubXOR) {
+				// if currentFrameIndex is not set we have to start a new
+				// frame here
+				if (currentHubFrameIndex < 0) {
+					// init current frame index at beginning
+					currentHubFrameIndex = 0;
+					// and copy this first byte in the frame
+					hubFrame[currentHubFrameIndex++] = b;
+				}
+				// otherwise we were already collecting a frame so this
+				// indicates we are at the end now. At this point a frame is
+				// available that we can send over.
+				else if (currentHubFrameIndex == SIZE_HUB_FRAME - 1) {
+					// just complete the frame we were collecting
+					hubFrame[currentHubFrameIndex] = b;
+					// this way the length is confirmed
+					handleHubDataFrame(hubFrame);
+					// once information is handled we can reset the frame
+					hubFrame = new int[SIZE_HUB_FRAME];
+					currentHubFrameIndex = 0;
+					// unlike the telemetry frames where 126 is on each side of
+					// the frame (126, x, y, 126, 126, x, y, 126) the 94 bit of
+					// the hub frame is only a single bit in between (94, x, y,
+					// 94, x, y, 94) so we need to reuse that last 94 bit
+					// encountered as the beginning of our next frame
+					hubFrame[currentHubFrameIndex++] = b;
+				}
+				// if for some reason we got 2 0x5e bytes after each other
+				// or the size of the frame was different we can't do
+				// anything with the previous collected information. We can
+				// log a debug message and drop the frame to start over
+				// again.
+				else {
+					// log debug info here
+					Logger.d(TAG, "Start/stop byte at wrong position: 0x"
+							+ Integer.toHexString(b) + " frame so far: "
+							+ Arrays.toString(hubFrame));
+					currentHubFrameIndex = 0;
+					hubFrame = new int[SIZE_HUB_FRAME];
+					hubFrame[currentHubFrameIndex++] = b;
+				}
+			}
+			// otherwise we are handling a valid byte that has to be put in
+			// the frame we are collecting. But only when we are currently
+			// working on a frame!
+			else if (currentHubFrameIndex >= 0
+					&& currentHubFrameIndex < SIZE_HUB_FRAME - 1) {
+				hubFrame[currentHubFrameIndex++] = b;
+			}
+			// finally it's possible that we receive bytes without being in
+			// a frame, just discard them for now
+			else {
+				// log debug info here
+				Logger.d(TAG, "Received data outside frame, dropped byte: 0x"
+						+ Integer.toHexString(b));
+			}
+			// make sure to unset the xor flag at this point
+			hubXOR = false;
+		}
 	}
 
 	/* (non-Javadoc)
@@ -613,7 +490,7 @@ public class FrSkyHub extends Hub{
 	@Override
 	public void addUserBytes(byte[] bytes) {
 		// FIXME incomplete
-		
+		throw new RuntimeException("Not Implemented");
 	}
 
 }
